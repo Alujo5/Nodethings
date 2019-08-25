fd7ada75501536dda26b25e0288053f995fac296
Alujo5 <neroneco9@gmail.com>
2019-08-25 01:38:33 +0000

    user input

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,26 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Lagos', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+const place = process.argv[2]
+
+if(!place){
+    return console.log('Please what location do you want? ')
+}
+
+geocode(place, (error, data) => {
+
+    if(error){
+        return console.log(error)
+    }
+
+    forecast( data.longitude , data.latitude, (error, forecastData) => {
+        if(error){
+            return console.log(error)
+        }
+
+        console.log(data.location)
+        console.log(forecastData)
+    })
+
 })
 
-forecast( 44.1545 , -75.7088, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
