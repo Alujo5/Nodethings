a8d5ec7c107a72719129cd0deac2b24d1f8c2daa
Alujo5 <neroneco9@gmail.com>
2019-09-07 23:28:15 +0000

    1st server/get

---
+++ web-server/src/app.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const express = require('express')
+
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
+
+const app = express()
+
+app.get('', (req, res) =>{
+   res.send('<h1>Weather</h1>')
+})
+
+app.get('/help', (req, res) => {
+   res.send({
+       name: 'Andrew',
+       age: 27
+   })
+})
+
+app.get('/about', (req, res) => {
+   res.send('<h1>About</h1>')
+})
+
+app.get('/weather', (req, res) => {
+   res.send({
+       forecast: 'It is raining',
+       location: 'Montreal'
+   })
+})
+
+app.listen(3000, () =>{
+   console.log('Server is up on port 3000.')
+})
