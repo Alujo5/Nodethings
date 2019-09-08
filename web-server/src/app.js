b501d071eaea5496aa96d1b0dec60302c903e587
Alujo5 <neroneco9@gmail.com>
2019-09-08 00:30:20 +0000

    express static/serving folder

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,24 +2,13 @@ const path = require('path')
 const express = require('express')
 
 console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
+
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) =>{
-   res.send('<h1>Weather</h1>')
-})
+app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res) => {
-   res.send({
-       name: 'Andrew',
-       age: 27
-   })
-})
-
-app.get('/about', (req, res) => {
-   res.send('<h1>About</h1>')
-})
 
 app.get('/weather', (req, res) => {
    res.send({
